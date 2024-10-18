f2b3061b4583a53b209b989bfdb56a64af2420b0
MDSHUJAUDDIN <mohammadshujauddin1210@gmail.com>
2024-10-18 22:53:46 +0530

    Ag-grid react added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import GridExample from './components/GridExample'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <>
-      <div className="flex justify-center items-center h-[100vh]">
-     <h1>AG-Grid</h1>
+    
+     <div className='flex-col justify-center items-center h-[100vh] bg-slate-300'>
+      <h1 className='text-center p-4 text-xl font-bold'>AG-Grid</h1>
+      <GridExample  />
     </div>
-    </>
   )
 }
 
