c96bfc936f265858895cf1518fd69b2878df6677
MDSHUJAUDDIN <mohammadshujauddin1210@gmail.com>
2024-10-18 22:01:46 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-[100vh]">
+     <h1>AG-Grid</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
