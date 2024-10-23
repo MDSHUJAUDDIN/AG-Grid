448aa85b191e0dd6c0d25001a62345739784c32c
Uday <uday66220@gmail.com>
2024-10-23 21:00:19 +0530

    "Full row editing"

--- src/components/GridExample.tsx
+++ src/components/GridExample.tsx
@@ -1,64 +1,81 @@
-// Theme
-import { ColDef } from "ag-grid-community";
+import { CellValueChangedEvent, ColDef, RowValueChangedEvent } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 // React Grid Logic
 import "ag-grid-community/styles/ag-grid.css";
 // Core CSS
 import "ag-grid-community/styles/ag-theme-quartz.css";
-import React, {  useCallback, useRef, useState } from "react";
-
+import { useEffect, useState,useMemo, useCallback } from "react";
 
 // Row Data Interface
 interface IRow {
-  make: string;
-  model: string;
-  price: number;
-  electric: boolean;
+  entityName: string;
+  parentID: string;
+  status: string;
+  countryInc: string;
+  entityType: string;
+  federalID: number;
+  functionalCurrency: string;
+  dateInc: string;
+  primaryContact: string;
 }
 
 // Create new GridExample component
 const GridExample = () => {
 
-  // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState<IRow[]>([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
-    { make: "Fiat", model: "500", price: 15774, electric: false },
-    { make: "Nissan", model: "Juke", price: 20675, electric: false },
-  ]);
+  const [rowData, setRowData] = useState<IRow[]>([]);
 
   // Column Definitions: Defines & controls grid columns.
-  const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
-    { field: "make" ,filter:true , floatingFilter:true},
-    { field: "model",  filter:true , floatingFilter:true},
-    { field: "price", filter:true , floatingFilter:true},
-    { field: "electric"},
+  const [colDefs] = useState<ColDef<IRow>[]>([
+
+    { field: "entityName", headerName: "Entity Name", filter: true },
+    { field: "parentID", headerName: "Parent ID", filter: true  },
+    { field: "status", headerName: "Status" , filter: true },
+    { field: "countryInc", headerName: "Country Inc.", filter: true  },
+    { field: "entityType", headerName: "Entity Type", filter: true  },
+    { field: "federalID", headerName: "Federal ID", filter: true  },
+    { field: "functionalCurrency", headerName: "Functional Currency" , filter: true },
+    { field: "dateInc", headerName: "Date Inc.", filter: true  },
+    { field: "primaryContact", headerName: "Primary Contact" , filter: true },
   ]);
 
   const defaultColDef: ColDef = {
     flex: 1,
+    editable: true,
   };
   
+  //code for editing the cells
+  //'tab' makes the cursor move to right cell in the editing row
+  //'shift' + 'tab' makes the cursor move to left cell in the editing row
+
+  const onCellValueChanged = useCallback((event: CellValueChangedEvent) => {
+    console.log('onCellValueChanged: ' + event.colDef.field + ' = ' + event.newValue);
+}, []);
+
+  useEffect(() => {
+    fetch("http://localhost:3004/users")
+      .then((response) => response.json())
+      .then((rowData) => setRowData(rowData))
+      .catch((error) => console.error("Error fetching data:", error));
+      
+  }, []);
+
   // Container: Defines the grid's theme & dimensions.
-  
-  
   return (
-    <div
-      className={
-        "ag-theme-quartz p-4"
-      }
-      style={{ width: "100%", height: "340px" }}
-    >
+    <div className="ag-theme-quartz" style={{ padding: "16px", width: "100%", height: "90vh" }}>
       <AgGridReact
         rowData={rowData}
         columnDefs={colDefs}
         defaultColDef={defaultColDef}
-        
-      />
+        pagination ={true}
+        editType={'fullRow'}
+        onCellValueChanged={onCellValueChanged}
+        />
     </div>
   );
 };
+
+
+
+
 
 export default GridExample;
