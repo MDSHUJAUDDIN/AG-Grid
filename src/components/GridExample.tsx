38cebcb8c8a6c80fe8546336dcfc4aa503a26e2f
Uday <uday66220@gmail.com>
2024-10-25 19:28:19 +0530

    "commited after resolve"

--- src/components/GridExample.tsx
+++ src/components/GridExample.tsx
@@ -1,11 +1,12 @@
 // Theme
-import { ColDef } from "ag-grid-community";
+import { CellValueChangedEvent,ColDef, DataTypeDefinition, GetRowIdParams, ModuleRegistry, RowValueChangedEvent, ValueFormatterParams } from "ag-grid-community";
+
 import { AgGridReact } from "ag-grid-react";
 // React Grid Logic
 import "ag-grid-community/styles/ag-grid.css";
 // Core CSS
 import "ag-grid-community/styles/ag-theme-quartz.css";
-import React, { useEffect, useState,useMemo } from "react";
+import React, { useEffect, useState,useMemo, useCallback, useRef } from "react";
 
 // Row Data Interface
 interface IRow {
@@ -23,27 +24,53 @@ interface IRow {
 // Create new GridExample component
 const GridExample = () => {
 
+  const gridRef = useRef<AgGridReact | null>(null);
   const [rowData, setRowData] = useState<IRow[]>([]);
 
   // Column Definitions: Defines & controls grid columns.
   const [colDefs] = useState<ColDef<IRow>[]>([
-   
+
+    { field: "entityName", headerName: "Entity Name", filter: true ,pinned :"left",tooltipField:"entityName"},
     { field: "parentID", headerName: "Parent ID", filter: true  },
-    { field: "status", headerName: "Status" , filter: true },
-    { field: "countryInc", headerName: "Country Inc.", filter: true  },
+    { field: "status", headerName: "Status" , filter: true,editable:true,
+        cellRenderer:'agCheckboxCellRenderer',
+        cellEditor: 'agCheckboxCellEditor',
+    },
+    { field: "countryInc", headerName: "Country Inc.", filter: true },
     { field: "entityType", headerName: "Entity Type", filter: true  },
-    { field: "entityName", headerName: "Entity Name", filter: true, pinned :"left",tooltipField:"entityName" },
     { field: "federalID", headerName: "Federal ID", filter: true  },
-    { field: "functionalCurrency", headerName: "Functional Currency" , filter: true },
-    { field: "dateInc", headerName: "Date Inc.", filter: true  },
+    { field: "functionalCurrency", headerName: "Functional Currency" , filter: true,editable:true,
+      cellEditor: 'agSelectCellEditor',
+            cellEditorParams: {
+                values: ['AUD','CAD','USD'],
+            },
+    },
+    { field: "dateInc", headerName: "Date Inc.", filter: true,editable:true,
+      cellEditor: 'agDateCellEditor',
+        cellEditorParams: {
+            min: '01-01-2000',
+            max: '31-12-2024',
+        }
+      },
     { field: "primaryContact", headerName: "Primary Contact" , filter: true },
+    
   ]);
 
-  const rowSelection = useMemo(() => {
-    return{
-          mode :"multiRow",
-    }
-  })
+
+  const defaultColDef: ColDef = {
+    flex: 1,
+    editable: true,
+    filter: true,
+    sortable:true,
+  };
+
+const onCellValueChanged = useCallback((event: CellValueChangedEvent) => {
+     console.log('onCellValueChanged: ' + event.data.colDefs);
+ }, []);
+// //row edit
+const onRowValueChanged = useCallback((event: RowValueChangedEvent) => {
+  const data = event.data;
+}, []);
 
   useEffect(() => {
     fetch("http://localhost:3004/users")
@@ -52,9 +79,7 @@ const GridExample = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  const defaultColDef: ColDef = {
-    flex: 1,
-  };
+  
 
   // Container: Defines the grid's theme & dimensions.
   return (
@@ -64,7 +89,12 @@ const GridExample = () => {
         columnDefs={colDefs}
         defaultColDef={defaultColDef}
         pagination ={true}
-        rowSelection = {rowSelection}
+        onCellValueChanged={onCellValueChanged}
+        onRowValueChanged={onRowValueChanged}
+        editType={'fullRow'}
+        rowSelection = {
+          {mode :"multiRow",
+    }}
       />
     </div>
   );
