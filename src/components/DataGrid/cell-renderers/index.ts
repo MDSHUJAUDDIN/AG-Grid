0d5b1ae2d4b78c9cdfbfc9c8811a462aa669f580
Shaik7013 <shabaz7013@gmail.com>
2024-11-07 15:46:54 +0530

    updated code with CountryCell Render

--- src/components/DataGrid/cell-renderers/index.ts
+++ src/components/DataGrid/cell-renderers/index.ts
@@ -1,7 +1,9 @@
 import { StatusCellRenderer } from "./StatusCellRenderer";
+import { CountryCellRenderer } from "./CountryCellRenderer";
 import type { CustomCellRendererProps } from "@ag-grid-community/react";
 import { type FunctionComponent } from "react";
 
 export const renderers: Record<string, FunctionComponent<CustomCellRendererProps>> = {
-    "StatusCellRenderer": StatusCellRenderer
+    "StatusCellRenderer": StatusCellRenderer,
+    "CountryCellRenderer": CountryCellRenderer,
 }
